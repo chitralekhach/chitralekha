8528b0c454545e9341801c4d8ea775ecd8ec894b
chitralekha <lekhachitra412@gmail.com>
2019-02-22 16:29:05 -0600

    commits

---
+++ test/test.js
@@ -0,0 +1,8 @@
+QUnit.test( "Testing calculateArea function with several sets of inputs", function( assert ){
+    var res=myfunction(3);
+    assert.equal(res,"9", "Tested with small positive number");
+    assert.equal(myfunction(9),"81","Tested with positive number");
+    assert.equal(myfunction(6),"36",'Tested the number with wrong result.');
+    assert.equal(myfunction(255),"50625",'Tested with large positive number.');
+    assert.equal(myfunction(-6),"36",'Tested with negative number value.')
+})
